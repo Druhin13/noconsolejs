7107022fb706aa0eca1591b15923a60ee85af4df
Druhin Tarafder <contact@druh.in>
2024-09-16 11:56:49 +0530

    refactored

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-(function (global) {
-  function disableConsole() {
-    if (typeof console !== "undefined") {
-      const methods = ["log", "warn", "error", "info", "debug", "table"];
-      methods.forEach(function (method) {
-        console[method] = function () {};
-      });
-    }
-  }
-  if (typeof module !== "undefined" && module.exports) {
-    module.exports = disableConsole;
-  } else {
-    disableConsole();
-  }
-})(typeof window !== "undefined" ? window : global);
+!function(n){function e(){"undefined"!=typeof console&&["log","warn","error","info","debug","table"].forEach(function(n){console[n]=function(){}})}"undefined"!=typeof module&&module.exports?module.exports=e:e()}("undefined"!=typeof window?window:global);
